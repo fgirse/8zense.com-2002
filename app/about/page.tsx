02998b91e3f5f4b209913d7d6b185e911679c5c2
Frank Girse <fgirse@mac-mini-von-frank.home>
2024-01-30 22:00:26 +0100

    17er commit

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,23 +1,41 @@
-import React from 'react'
+
 import Image from 'next/image'
 import LogoEZ from "/public/assets/images/LogoEZ.png";
+import React from 'react'
 
 const page = () => {
   return (
-  
-    <div className='flex-1 h-screen bg-[url("/assets/images/room.webp")] bg-no-repeat bg-cover bg-center'>
+    <>
+    <section className=' flex-1 bg-[url("/assets/images/room.webp")] bg-cover bg-no-repeat bg-center '>
+    <h1 className="text-3xl text-amber-500 text-center ">About</h1>
+    <div className='mb-5 flex flex-col items-center yustify-center'>
+    <Image className="mt-5 rounded-full h-auto w-36 max-w-sm  " src={LogoEZ} alt="Logo 8zense.com"/>
+    </div>
+    <div className=" grid grid-cols-5 grid-rows-5 gap-4">
+      <div className="col-span-5 row-span-2 ">
+      <h1 className="mt-40 text-[3.33rem] leading-10 text-zinc-200 font-black text-center">Was ist 8Zense.com ?</h1> 
+      </div>
+      <div className="px-3 col-span-5 row-span-2 row-start-3">
+      <p className="bg-black/30 -mt-36 py-3 border rounded-xl px-5 text-[-.5rem] font-bold text-zinc-200 text-justify  "><span className='text-orange-300'></span>8zenSe.com ist <span className='text-yellow-600'>Fernanda Pereida</span>. 8zense enstand aus einer Intuition, dass Fernanda  immer häufiger sich die Frage stellte ob gutes Design nur denjenigen Menschen zusteht die dies sich auch leisten können. Als studierte Innenarchitektin hat Fernanda unzählige Projekte betreut und die Ingredentien  Beton - Holz - Stahl  umd Glas haben sich als die favorisierten Werkstoffe ihres Schaffens rund um gutes, ansprechendes  und zeitloses Design entwickelt. Es war immer Fernandas Wunsch die Symbiose dieser Werkstoffe zu kombinieren und mit ihnen zu experimentieren. Das Ergebnis ist 'Exclusivität und Individualität ausgezeichnet durch einzigartiges Desingn</p>                                                                                                                                                                             
 
-         <h1  className='text-center uppercase font-black text-1xl text-yellow-600' >about</h1>
-         <div className='flex flex-col jusify-center items-center'>
-          <Image className="rounded-full h-auto w-1/4 max-w-sm  " src={LogoEZ}  alt="Logo 8zense.com"/>
-         </div>
-         <div className="mt-36 container grid grid-cols-1 grid-rows-3">
-          <h1 className="text-5xl text-zinc-200 font-black text-[3rem] text-center">Was ist 8Zense.com ?</h1> 
-         <p className=" rounded-xl px-1 py-1 bg-black/50 text-[-.5rem] font-bold text-zinc-200 text-justify -mt-36"><span className='text-orange-300'></span>8zenSe.com ist <span className='text-yellow-600'>Fernanda Pereida</span>. 8zense enstand aus einer Intuition, dass Fernanda  immer häufiger sich die Frage stellte ob gutes Design nur denjenigen Menschen zusteht die dies sich auch leisten können. Als studierte Innenarchitektin hat Fernanda unzählige Projekte betreut und die Ingredentien  Beton - Holz - Stahl  umd Glas haben sich als die favorisierten Werkstoffe ihres Schaffens rund um gutes, ansprechendes  und zeitloses Design entwickelt. Es war immer Fernandas Wunsch die Symbiose dieser Werkstoffe zu kombinieren und mit ihnen zu experimentieren. Das Ergebnis ist 'Exclusivität und Individualität ausgezeichnet durch einzigartiges Desingn</p>
-      
-      </div></div>
+
+                          
+        <h1 className=" mt-3 text-3xl py-3 text-white bg-orange-500 rounded-xl text-center font-bold">Education
+        
+        
+        </h1>
+        <h2 className='text-white tgext-2xl text-center'>Curriculum                                                                                                                                </h2>
+        
+      </div>
+    </div>
+    <div className='bg-orange-400'>
+
+    </div>
+    </section>
+    </>
     
   )
 }
-
+ 
 export default page
+        
