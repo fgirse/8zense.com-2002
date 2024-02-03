1b844de5082ba2053703310441c8b97708e9f35a
Frank Girse <fgirse@mac-mini-von-frank.home>
2024-02-03 05:17:04 +0100

    26er commit

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -8,7 +8,7 @@ export default function Page() {
     <>
 
     <div className="bg-stone-700 py-7 flex flex-col gap-y-5 items-center justify-center">
-      <Image src={Locked} alt="Illustration Schloss"/>
+      <Image className="w-36" src={Locked} alt="Illustration Schloss"/>
     <p className="mt-rtext-xs text-center text-slate-200">Dieser Inhalt ist nur nach Anmeldung bzw. Registrierung zugänglich!</p>
       <h1 className="text-sm md:text-sm lg:text-[0.66rem] text-center text-slate-500">
 
