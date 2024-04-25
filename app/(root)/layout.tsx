fd9be5c43101d7cfd1952cc163e9e098aaae95c2
Frank Girse <fgirse@mac-mini-von-frank.home>
2024-04-25 17:52:07 +0200

    commit 96 er

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -2,25 +2,21 @@ import Footer from "@/components/share/Footer";
 import Header from "@/components/share/Header";
 import ScrollToTop from "@/components/BackToTop/ScrollToTop";
 import Newsletter from "@/components/share/Newsletter";
-import FlipMenu from"@/components/share//FlipMenu";
-import styles from './styles.module.css'
+import styles from "./styles.module.css";
 import { HeadManagerContext } from "next/dist/shared/lib/head-manager-context.shared-runtime";
 
 export default function HomeLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
-    
-      <>
+    <>
       <Header />
       <main className={styles.main}>{children}</main>
       <ScrollToTop />
       <Newsletter />
       <Footer />
-      </>
-      
-    
-  )
+    </>
+  );
 }
